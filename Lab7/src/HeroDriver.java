bd3c330c8f379633803d7359e5057c0e89262341
colto <colto@10.17.203.117>
2018-10-26 09:42:25 -0600

    new update

--- Lab7/src/HeroDriver.java
+++ Lab7/src/HeroDriver.java
@@ -1,13 +1,24 @@
+import java.io.FileNotFoundException;
 
-
-
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 //made October 19, 2018
-public class HeroDriver {
+public class HeroDriver extends Application{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
 
 	}
+	
+	
+	public void start(Stage scene) throws Exception, FileNotFoundException
+	{
+	
 
+		
+		
+		
+		
+	}
 }
