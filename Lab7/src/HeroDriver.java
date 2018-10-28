cf6c0137f6a67c421d27b61e70ebef330dbec09d
kobesagami <kobesagami@10.17.204.10>
2018-10-28 13:02:33 -0600

    Added a start screen.

--- Lab7/src/HeroDriver.java
+++ Lab7/src/HeroDriver.java
@@ -1,14 +1,21 @@
 import java.io.FileNotFoundException;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 //made October 19, 2018
 public class HeroDriver extends Application
 {
+	Scene currentScene;
+	Player player1;
+	
+	Stage stage;
+
 	public static void main(String[] args) 
 	{
 		launch(args);
@@ -17,12 +24,31 @@ public class HeroDriver extends Application
 	
 	public void start(Stage primaryStage) throws Exception, FileNotFoundException
 	{
-		Group myGroup = new Group();
-		Player player1 = new Player(new Scene(myGroup, 600, 600), myGroup);
-				
-		primaryStage.setTitle("My Game");
+		// Start screen
+		Text startScreenText = new Text("Press the button to begin!");
+		startScreenText.relocate(220, 200);
 		
-		primaryStage.setScene(player1.playermove());
+		Button startScreenButton = new Button("Go!");
+		startScreenButton.relocate(280, 230);
+		startScreenButton.setOnAction(this::startButton);
+		
+		Group startScreenGroup = new Group(startScreenText, startScreenButton);
+		Scene startScreenScene = new Scene(startScreenGroup, 600, 600);
+		currentScene = startScreenScene;
+		
+		// Game
+		Group myGroup = new Group();
+		player1 = new Player(new Scene(myGroup, 600, 600), myGroup);
+				
+		// Stage
+		this.stage = primaryStage;
+		primaryStage.setTitle("My Game");
+		primaryStage.setScene(currentScene);
 		primaryStage.show();
+	}
+	
+	public void startButton(ActionEvent args)
+	{
+		stage.setScene(player1.playermove());
 	}
 }
