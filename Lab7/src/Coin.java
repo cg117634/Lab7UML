c877d088ae781d8096b52438f0636502b1d62491
cg117634 <35784792+cg117634@users.noreply.github.com>
2018-11-02 10:16:59 -0600

    Update for Coin

--- Lab7/src/Coin.java
+++ Lab7/src/Coin.java
@@ -1,19 +1,24 @@
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 public class Coin {
 	private Rectangle2D psb = Screen.getPrimary().getVisualBounds();
 	private ImageView imgView;
+	private Scene scene;
+	private Stage stage;
 	
-	public Coin()
+	public Coin(Scene primaryScene, Image inputImage, Stage stage)
 	{
 		
-		imgView = new ImageView(new Image("file:src/coin.png"));
-		imgView.relocate(psb.getWidth() * .1, psb.getHeight() * .1);
+		this.imgView = new ImageView(new Image("file:src/coin.png"));
+		imgView.relocate(psb.getWidth() * .3, psb.getHeight() * .3);
 		imgView.setFitHeight(30);
 		imgView.setFitWidth(30);
+		this.stage = stage;
 	}
 	
 	public ImageView getImgView()
